b1e1f32de6993ea7df8797242febf501f9198b15
Jake <jasmin.keller@hftm.ch>
2022-03-19 14:17:59 +0100

    initial commit

---
+++ init_sql_scripts/init.sql
@@ -0,0 +1,9 @@
+create table country_and_capitals_jake (
+	country text,
+	capital text
+);
+
+insert into country_and_capitals_jake(country, capital) values ('India', 'Delhi');
+insert into country_and_capitals_jake(country, capital) values ('China', 'Beijing');
+insert into country_and_capitals_jake(country, capital) values ('Bhutan', 'Thimpu');
+insert into country_and_capitals_jake(country, capital) values ('Russia', 'Moscow');
