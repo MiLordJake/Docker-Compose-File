a9d13ce0f2d043c23d868c5f46b8542191144c45
Jake <jasmin.keller@hftm.ch>
2022-03-19 14:31:42 +0100

    fix table name in index.js of backend_jake

--- backend_jake/index.js
+++ backend_jake/index.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 app.get('/data', function(req, res) {
-    pool.query('SELECT country, capital from country_and_capitals', [], (err, result) => {
+    pool.query('SELECT country, capital from country_and_capitals_jake', [], (err, result) => {
         if (err) {
             return res.status(405).jsonp({
                 error: err
